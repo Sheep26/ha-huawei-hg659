cee1e38d219327514dd89fa190de2e50ac02452a
Sheep26 <crabeykiller@gmail.com>
2025-04-14 19:26:07 +1200

    remove device_tracker.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,10 @@ from typing import Final
 
 _LOGGER = logging.getLogger(__name__)
 
+DOMAIN = "huawei_hg659"
+
 async def async_setup(hass, config):
+    #hass.states.async_set("hello_state.world", "Paulus")
+    
     # Setup component.
     return True
