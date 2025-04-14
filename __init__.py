43166b9b17a8b5d29c8986b33df2c53f51259ff6
Sheep26 <crabeykiller@gmail.com>
2025-04-14 19:15:32 +1200

    init

--- __init__.py
+++ __init__.py
@@ -1,2 +1,10 @@
-from . import device_tracker
-from .device_tracker import *
+# HG659 home assistant integration.
+import logging
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    # Setup component.
+    pass
