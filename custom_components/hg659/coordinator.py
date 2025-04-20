8fdb4d12b9deef539074ff993d7f95e6fe1cbef2
Sheep26 <crabeykiller@gmail.com>
2025-04-20 17:27:12 +1200

    prep for hacs

--- custom_components/hg659/coordinator.py
+++ custom_components/hg659/coordinator.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 from dataclasses import dataclass
 from datetime import timedelta
 import logging
-from typing import Any
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
